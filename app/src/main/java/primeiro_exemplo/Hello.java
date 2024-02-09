b4621b72953e03dd7a305bf8315145e3d6baa910
Elisa-Kirnew <elisakirnew@gmail.com>
2024-02-09 15:08:57 +0000

    Variaveis

--- app/src/main/java/primeiro_exemplo/Hello.java
+++ app/src/main/java/primeiro_exemplo/Hello.java
@@ -2,6 +2,10 @@ package primeiro_exemplo;
 
 public class Hello {
     public static void main(String[] args) {
+        int cont = 10;
+        String nome = "Elisa";
         System.out.println("Olá Mundo !!!");
+
+        System.out.println(cont + " : " + nome);
     }
 }
